417adcd72ace5cc7b7a544c12d626cac290579d5
Rafal Mi <rafal.m4@gmail.com>
2020-12-19 02:01:45 +0100

    Run in Kotlin

---
+++ README.md
@@ -0,0 +1,5 @@
+# Bottom
+
+Based on
+
+https://droidbyme.medium.com/android-bottom-sheet-7e9cfcec6427
---
+++ app/src/main/java/com/example/bottom/MainActivity.kt
@@ -0,0 +1,84 @@
+package com.example.bottom
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Button
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetBehavior.BottomSheetCallback
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+
+
+class MainActivity : AppCompatActivity() {
+
+    private var sheetBehavior: BottomSheetBehavior<*>? = null
+    private var bottom_sheet: LinearLayout? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+
+        bottom_sheet = findViewById(R.id.bottom_sheet);
+        sheetBehavior = BottomSheetBehavior.from(bottom_sheet as LinearLayout);
+
+        val btn_bottom_sheet = findViewById<Button>(R.id.btn_bottom_sheet)
+        btn_bottom_sheet.setOnClickListener {
+            val behavior = sheetBehavior as BottomSheetBehavior
+            if (behavior.getState() != BottomSheetBehavior.STATE_EXPANDED) {
+                behavior.setState(BottomSheetBehavior.STATE_EXPANDED)
+                btn_bottom_sheet.text = "Close sheet"
+            } else {
+                behavior.setState(BottomSheetBehavior.STATE_COLLAPSED)
+                btn_bottom_sheet.text = "Expand sheet"
+            }
+        }
+
+        (sheetBehavior as BottomSheetBehavior<*>).addBottomSheetCallback(object : BottomSheetCallback() {
+            override fun onStateChanged(view: View, newState: Int) {
+                when (newState) {
+                    BottomSheetBehavior.STATE_HIDDEN -> {
+                    }
+                    BottomSheetBehavior.STATE_EXPANDED -> {
+                        btn_bottom_sheet.text = "Close Sheet"
+                    }
+                    BottomSheetBehavior.STATE_COLLAPSED -> {
+                        btn_bottom_sheet.text = "Expand Sheet"
+                    }
+                    BottomSheetBehavior.STATE_DRAGGING -> {
+                    }
+                    BottomSheetBehavior.STATE_SETTLING -> {
+                    }
+                }
+            }
+
+            override fun onSlide(view: View, v: Float) {}
+        })
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        return when (item.itemId) {
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+}
